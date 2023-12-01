f7174e5cf592727a0a748013de76bf17d9acae82
sk2597 <smruti.karade25@gmail.com>
2023-12-01 22:37:19 +0530

    class Node and SinglyLinkedLists

--- linkedList.py
+++ linkedList.py
@@ -1,2 +1,10 @@
 """Singly linkedList"""
 
+class Node:
+  def __init__(self, item = None, next = None):
+    self.item = item
+    self.next = next
+
+class SinglyLinkedList:
+  def __init__(self, start = None):
+    self.start = start
