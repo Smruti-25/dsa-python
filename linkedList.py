5e7a37dc4ed721696469f082433e03827156b16a
sk2597 <smruti.karade25@gmail.com>
2023-12-01 23:15:56 +0530

    insert at start and last

--- linkedList.py
+++ linkedList.py
@@ -10,3 +10,18 @@ class SinglyLinkedList:
     self.start = start
   def isEmpty(self):
     return self.start == None
+  def insert_at_start(self, data = None):
+    new_node = Node(data, self.start)
+    self.start = new_node
+  def insert_at_last(self, data = None):
+    new_node = Node(data, None)
+    if not self.isEmpty():
+      currentNode = self.start
+      while currentNode.next is not None:
+        currentNode = currentNode.next
+      currentNode.next = new_node
+    else:
+      self.start = new_node
+  def search(self, item = None):
+
+
